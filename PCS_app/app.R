875b8dd88728963b8505f90f873215ded33af2f7
Samuel Coavoux <samuel.coavoux@gmail.com>
2019-05-16 16:25:18 +0200

    ajouté champ ; ajouté graph ; ajouté légende ; ajouté download ; régression sur filtrage des variables par thème

--- PCS_app/app.R
+++ PCS_app/app.R
@@ -2,19 +2,34 @@ library(shiny)
 library(tidyverse)
 library(questionr)
 library(janitor)
+library(here)
 
-vars <- read_csv("data/variables.csv")
-datasets <- read_csv("data/datasets.csv")
-d <- map(datasets$path, read_csv)
+vars <- read_csv(here("PCS_app", "data", "variables.csv"))
+datasets <- read_csv(here("PCS_app", "data", "datasets.csv"))
+d <- map(here("PCS_app", datasets$path), read_csv)
 names(d) <- datasets$dataset
+
 # TEMPORAIRE: devrait se faire près appli
 d$eec <- rename(d$eec, 
                 gsp = "gsp_tous",
                 csi = "csi_tous",
                 csd = "csd_tous")
 
+d <- map(d, function(df){
+  mutate(df, gsp = factor(gsp, levels = c("Agriculteurs exploitants",
+                                          "Artisans, commerçants, chefs d'entreprise", 
+                                          "Cadres et professions intellectuelles supérieures",
+                                          "Professions intermédiaires",
+                                          "Employés",
+                                          "Ouvriers", 
+                                          "Inactifs ayant déjà travaillé", 
+                                          "Autres inactifs")))
+})
+
 ## Fonction créant les tableaux
 tab <- function(df, var, pcs){
+  df <- filter(df, !is.na(!! sym(pcs)))
+  
   if(is.character(df[[var]])) {
     tb <- tabyl(df, !! sym(pcs), !! sym(var)) %>% 
       adorn_totals(c("row", "col")) %>%
@@ -38,7 +53,41 @@ tab <- function(df, var, pcs){
   return(tb)  
 }
 
+theme_set(theme_bw())
+
 ## Fonction créant les graphiques
+
+graph <- function(df, var, pcs){
+  
+  df <- filter(df, !is.na(!! sym(pcs)))
+  
+  if(is.character(df[[var]])) {
+    gr <- ggplot(df, aes(x = !! sym(pcs), 
+                         y = (..count..)/sum(..count..),
+                         fill = !! sym(var))) +
+      geom_bar(position = "dodge") +
+      coord_flip()
+  }
+  else if(is.numeric(df[[var]])) {
+    gr <- ggplot(df, aes(x = !! sym(pcs), y = !! sym(var))) +
+      geom_boxplot() +
+      coord_flip()
+  }
+  return(gr)  
+}
+
+filter_champ <- function(df, champ){
+  if(champ == "Ensemble de la population ayant déjà travaillé") {
+    df <- filter(df, statut_activite %in% c("Actifs occupés", "Inactifs", "Chômeurs"))
+  } else if(champ == "Ensemble des actifs"){
+    df <- filter(df, statut_activite %in% c("Actifs occupés", "Chômeurs"))
+  } else if(champ == "Actifs occupés"){
+    df <- filter(df, statut_activite %in% c("Actifs occupés"))
+  } else if(champ == "Chômeurs et inactifs"){
+    df <- filter(df, statut_activite %in% c("Inactifs", "Chômeurs"))
+  }
+  return(df)
+}
 
 ## UI
 ui <- fluidPage(
@@ -52,7 +101,8 @@ ui <- fluidPage(
       selectInput("theme", label = h3("Sélectionnez une thématique"), 
                   choices = unique(vars$theme)),
       # les autres choix, dynamiques, soit dans server
-      uiOutput("varSelection"),
+      selectInput("variable", label = h3("Sélectionnez une variable"), 
+                  choices = unique(vars$variable)),
       selectInput("pcslevel", label = h3("Sélectionnez un niveau de PCS"), 
                   choices = list("Groupe socio-professionnel" = "gsp",
                                  "Catégorie socio-professionnelle" = "csi",
@@ -60,30 +110,64 @@ ui <- fluidPage(
                   selected = 1),
 
       selectInput("champ", label = h3("Sélectionnez un champ"), 
-                  choices = list("Ensemble de la population ayant déjà travaillé" = 1,
-                                 "Ensemble des actifs" = 2,
-                                 "Actifs occupés" = 3,
-                                 "Chômeurs et inactifs" = 4), 
+                  choices = list("Ensemble de la population ayant déjà travaillé",
+                                 "Ensemble des actifs",
+                                 "Actifs occupés",
+                                 "Chômeurs et inactifs"), 
                   selected = 1),
-      uiOutput("champSelection")
+      downloadButton("dlTable", "Télécharger le tableau"),
+      downloadButton("dlGraph", "Télécharger le graphique")
     ),
     
     # Zone de résultat
     mainPanel = mainPanel(
-      tableOutput(outputId = "table")
+      tableOutput(outputId = "table"),
+      plotOutput(outputId = "graph"),
+      textOutput(outputId = "source"),
+      textOutput(outputId = "champ")
     )
   )
 )
   
 ## Serveur
 server <- function(input, output){
-  output$varSelection <- renderUI({
-    selectInput("variable", label = h3("Sélectionnez une variable"), 
-                choices = vars$variable[vars$theme == input$theme])
-  })
-  output$table <- renderTable(tab(df = d[["eec"]], 
+  
+  # available_variables <- reactiveVal()
+  # observeEvent(input$theme, {
+  #   available_variables(unique(vars$variable[vars$theme == input$theme]))
+  #   print(paste0("Succesfully updated, new value: ", available_variables()))
+  # })
+
+  output$table <- renderTable(tab(df = filter_champ(df = d[[vars$dataset[vars$variable == input$variable]]],
+                                                    champ = input$champ), 
                                   var = input$variable, 
                                   pcs = input$pcslevel))
+  output$graph <- renderPlot(graph(df = filter_champ(df = d[[vars$dataset[vars$variable == input$variable]]],
+                                                   champ = input$champ), 
+                                 var = input$variable, 
+                                 pcs = input$pcslevel))
+  output$source <- renderText(paste0("Source : " , datasets$fullname[datasets$dataset == vars$dataset[vars$variable == input$variable]]))
+  output$champ  <- renderText(paste0("Champ : ", input$champ))
+  output$dlTable <- downloadHandler(
+    filename = "tableau.csv",
+    content = function(file) {
+      write_csv(tab(df = filter_champ(df = d[[vars$dataset[vars$variable == input$variable]]],
+                                      champ = input$champ), 
+                    var = input$variable, 
+                    pcs = input$pcslevel), 
+                file)
+    }
+  )
+  output$dlGraph <- downloadHandler(
+    filename = "graphique.png",
+    content = function(file) {
+      ggsave(file, 
+             graph(df = filter_champ(df = d[[vars$dataset[vars$variable == input$variable]]],
+                                     champ = input$champ), 
+                   var = input$variable, 
+                   pcs = input$pcslevel))
+    }
+  )
 }
 
 # Lancer l'application
